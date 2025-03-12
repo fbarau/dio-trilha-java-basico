d158be855b0076e3b6e37d08bc4f3950729b9ea0
Fernanda Barau <fernanda.barbosaaraujo@claro.com.br>
2025-03-12 17:47:12 -0300

    Projeto com exercícios para gerar doc java.

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author Fernanda barbosa
+ * @version 1.0
+ * @since 12/03/2025
+ */
+public class Calculadora {
+    /**
+     * Este método foi criado e deve ser utilizado para somar dois números
+     *
+     * @param numeroUm   este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return double o resultado deste método é a soma dos dois números.
+     */
+    public double somar(double numeroUm, double numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
