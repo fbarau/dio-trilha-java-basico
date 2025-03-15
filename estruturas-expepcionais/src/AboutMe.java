60ac363b3f404a2cbade78913f06d6fdf8aa098a
Fernanda Barau <fernanda.barbosaaraujo@claro.com.br>
2025-03-15 19:10:53 -0300

    Estruturas Excepcionais

--- estruturas-expepcionais/src/AboutMe.java
+++ estruturas-expepcionais/src/AboutMe.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        // criando o objeto scanner
+        //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome");
         String nome = scanner.next();
 
         System.out.println("Digite seu sobrenome");
-
         String sobrenome = scanner.next();
 
         System.out.println("Digite sua idade");
@@ -19,7 +18,8 @@ public class AboutMe {
         System.out.println("Digite sua altura");
         double altura = scanner.nextDouble();
 
-        // imprimindo os dados obtidos pelo usuario
+
+        //imprimindo os dados obtidos pelo usuario
         System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
